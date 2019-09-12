bdc45ceaed5e21feaec1c476db74f287f70fe6c4
pedrobarriosalten <pedrojose.barrios@alten.es>
2019-09-12 14:37:52 +0200

    Cambios en las variables de las Entidades

--- src/main/java/es/commerzbank/ice/embargos/domain/mapper/OrderingEntityMapper.java
+++ src/main/java/es/commerzbank/ice/embargos/domain/mapper/OrderingEntityMapper.java
@@ -17,9 +17,6 @@ public abstract class OrderingEntityMapper {
 		@Mapping(source = "codOrderingEntity", target = "codEntidadOrdenante"),
 		@Mapping(source = "nifEntity", target = "nifEntidad"),
 		@Mapping(source = "desEntity", target = "desEntidad"),
-		@Mapping(source = "indCgpj", target = "indCgpj"),
-		@Mapping(source = "indAeat", target = "indFormatoAeat"),
-		@Mapping(source = "indNorma63", target = "indNorma63"),
 		@Mapping(source = "communicatingEntity.code", target = "entidadesComunicadora.codEntidadPresentadora")
 	})
 	public abstract EntidadesOrdenante toEntidadOrdenante(OrderingEntity orderingEntity);
@@ -28,9 +25,6 @@ public abstract class OrderingEntityMapper {
 		@Mapping(source = "codEntidadOrdenante", target = "codOrderingEntity"),
 		@Mapping(source = "nifEntidad", target = "nifEntity"),
 		@Mapping(source = "desEntidad", target = "desEntity"),
-		@Mapping(source = "indCgpj", target = "indCgpj"),
-		@Mapping(source = "indFormatoAeat", target = "indAeat"),
-		@Mapping(source = "indNorma63", target = "indNorma63"),
 		@Mapping(source = "entidadesComunicadora.codEntidadPresentadora", target = "communicatingEntity.code"),
 		@Mapping(source = "entidadesComunicadora.desEntidad", target = "communicatingEntity.name")
 	})
@@ -38,10 +32,55 @@ public abstract class OrderingEntityMapper {
 
 	@AfterMapping
 	protected void setOrderingEntityAfterMapping(@MappingTarget OrderingEntity orderingentity, EntidadesOrdenante entidadesOrdenante) {
-		if (entidadesOrdenante.getIndActivo().equals(EmbargosConstants.IND_FLAG_YES)) {
+		if (entidadesOrdenante.getIndActivo().equals(EmbargosConstants.IND_FLAG_SI)) {
 			orderingentity.setActive(true);
 		} else {
 			orderingentity.setActive(false);
+		}
+		
+		if (entidadesOrdenante.getIndCgpj().equals(EmbargosConstants.IND_FLAG_SI)) {
+			orderingentity.setIndCgpj(true);
+		} else {
+			orderingentity.setIndCgpj(false);
+		}
+		
+		if (entidadesOrdenante.getIndFormatoAeat().equals(EmbargosConstants.IND_FLAG_SI)) {
+			orderingentity.setIndAeat(true);
+		} else {
+			orderingentity.setIndAeat(false);
+		}
+		
+		if (entidadesOrdenante.getIndNorma63().equals(EmbargosConstants.IND_FLAG_SI)) {
+			orderingentity.setIndNorma63(true);
+		} else {
+			orderingentity.setIndNorma63(false);
+		}
+	}
+	
+	@AfterMapping
+	protected void setEntidadOrdenanteAfterMapping(@MappingTarget EntidadesOrdenante entidadesOrdenante, OrderingEntity orderingentity) {
+		if (orderingentity.isActive()) {
+			entidadesOrdenante.setIndActivo(EmbargosConstants.IND_FLAG_SI);
+		} else {
+			entidadesOrdenante.setIndActivo(EmbargosConstants.IND_FLAG_NO);
+		}
+		
+		if (orderingentity.getIndCgpj()) {
+			entidadesOrdenante.setIndCgpj(EmbargosConstants.IND_FLAG_SI);
+		} else {
+			entidadesOrdenante.setIndCgpj(EmbargosConstants.IND_FLAG_NO);
+		}
+		
+		if (orderingentity.getIndAeat()) {
+			entidadesOrdenante.setIndFormatoAeat(EmbargosConstants.IND_FLAG_SI);
+		} else {
+			entidadesOrdenante.setIndFormatoAeat(EmbargosConstants.IND_FLAG_NO);
+		}
+		
+		if (orderingentity.getIndNorma63()) {
+			entidadesOrdenante.setIndNorma63(EmbargosConstants.IND_FLAG_SI);
+		} else {
+			entidadesOrdenante.setIndNorma63(EmbargosConstants.IND_FLAG_NO);
 		}
 	}
 }
