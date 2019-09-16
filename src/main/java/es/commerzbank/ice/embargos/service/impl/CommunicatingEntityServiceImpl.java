971dd0c68724d42cc7376d0b5446896a10aa4c3e
manuel.carrasco <manuel.carrasco@alten.es>
2019-09-16 16:57:36 +0200

    fix field name

--- src/main/java/es/commerzbank/ice/embargos/service/impl/CommunicatingEntityServiceImpl.java
+++ src/main/java/es/commerzbank/ice/embargos/service/impl/CommunicatingEntityServiceImpl.java
@@ -109,7 +109,7 @@ public class CommunicatingEntityServiceImpl implements CommunicatingEntityServic
 			public Predicate toPredicate(Root<EntidadesComunicadora> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
 				List<Predicate> predicates = new ArrayList<>();
 				
-				predicates.add(criteriaBuilder.equal(root.get(EntidadesComunicadora_.indActivo), EmbargosConstants.IND_FLAG_SI));
+				predicates.add(criteriaBuilder.equal(root.get(EntidadesComunicadora_.IND_ACTIVO), EmbargosConstants.IND_FLAG_SI));
 		
 				return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
 			}
