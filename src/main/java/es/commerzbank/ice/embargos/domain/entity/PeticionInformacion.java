d1c52e92a3536586e6b0e1020133cc8da09afa2d
alten_amassana <aniol.massana@alten.es>
2021-02-05 13:48:11 +0100

    fix

--- src/main/java/es/commerzbank/ice/embargos/domain/entity/PeticionInformacion.java
+++ src/main/java/es/commerzbank/ice/embargos/domain/entity/PeticionInformacion.java
@@ -4,19 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import es.commerzbank.ice.embargos.domain.entity.listener.PeticionInformacionListener;
 
@@ -32,6 +20,7 @@ import es.commerzbank.ice.embargos.domain.entity.listener.PeticionInformacionLis
 public class PeticionInformacion implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Transient
 	private int invoked = 0;
 	public int getNumInvocations() {
 		invoked++;
