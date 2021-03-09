c7cfccfebe52878eadb18553d15d288221ba5225
Daniel Borruel <daniel.borruelg@alten.es>
2021-03-09 16:30:49 +0100

    0014719: Email archivo outbox

--- src/main/java/es/commerzbank/ice/embargos/event/AlertStatusFileControl.java
+++ src/main/java/es/commerzbank/ice/embargos/event/AlertStatusFileControl.java
@@ -1,6 +1,7 @@
 package es.commerzbank.ice.embargos.event;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import es.commerzbank.ice.comun.lib.service.GeneralParametersService;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import es.commerzbank.ice.embargos.domain.entity.ControlFichero;
 import es.commerzbank.ice.embargos.repository.FileControlRepository;
+import es.commerzbank.ice.embargos.service.EmailService;
 import es.commerzbank.ice.embargos.utils.EmbargosConstants;
 
 @Service
@@ -27,16 +29,22 @@ public class AlertStatusFileControl {
 	@Autowired
 	private GeneralParametersService generalParameterService;
 	
+	@Autowired
+	private EmailService emailService;
+	
 	@Async
-	@Scheduled(fixedRate = 60000)
+	@Scheduled(fixedRate = 3600000)
 	public void alertStatusFileControl() {
 		try {
-			List<ControlFichero> list = fileControlRepository.findByCodEstadoAndFecha(EmbargosConstants.COD_ESTADO_CONTROL_FICHERO_GENERADO_SCHEDULED, 4);
+			int diasEmailNoLecturaFicheros = generalParameterService.loadIntegerParameter(EmbargosConstants.PARAMETRO_EMBARGOS_DIAS_EMAIL_NOLECTURA_FICHEROS);
+			List<ControlFichero> list = fileControlRepository.findByCodEstadoAndFecha(EmbargosConstants.COD_ESTADO_CONTROL_FICHERO_GENERADO_SCHEDULED, diasEmailNoLecturaFicheros);
 			
 			String folderNameAEATGenerated = generalParameterService.loadStringParameter(EmbargosConstants.PARAMETRO_EMBARGOS_FILES_PATH_AEAT_OUTBOX);
 			String folderNameN63Generated = generalParameterService.loadStringParameter(EmbargosConstants.PARAMETRO_EMBARGOS_FILES_PATH_NORMA63_OUTBOX);
 			
-			logger.info("Número de archivos encontrado: " + list.size());
+			logger.info("Número de control fichero encontrado sin leer de outbox: " + list.size());
+			
+			List<ControlFichero> listaEnvio = new ArrayList<ControlFichero>();
 			
 			for (ControlFichero controlFichero : list) {
 				File fileAEAT = new File(folderNameAEATGenerated, controlFichero.getNombreFichero());
@@ -44,8 +52,13 @@ public class AlertStatusFileControl {
 				if (fileAEAT.exists() || fileN63.exists())
 				{
 					logger.info("El fichero "+ controlFichero.getCodControlFichero() +" NO ha sido leído de la carpeta outbox.");
+					listaEnvio.add(controlFichero);
 				}
 			}
+			
+			logger.info("Número de archivos encontrado sin leer de outbox: " + listaEnvio.size());
+			
+			emailService.sendEmailUnreadFiles(listaEnvio);
 		}
 		catch (Exception e) {
 			logger.error("ERROR - AlertStatusFileControl - alertStatusFileControl() ", e);
--- src/main/java/es/commerzbank/ice/embargos/service/EmailService.java
+++ src/main/java/es/commerzbank/ice/embargos/service/EmailService.java
@@ -3,6 +3,7 @@ package es.commerzbank.ice.embargos.service;
 import java.util.List;
 
 import es.commerzbank.ice.comun.lib.util.ICEException;
+import es.commerzbank.ice.embargos.domain.entity.ControlFichero;
 import es.commerzbank.ice.embargos.domain.entity.ErrorTraba;
 
 public interface EmailService {
@@ -12,6 +13,8 @@ public interface EmailService {
 	public void sendEmailPetitionReceived(String fileName);
 
 	public void sendEmailFileError(List <ErrorTraba> errores, String nombreFichero, String rutaFichero) throws ICEException;
+	
+	public void sendEmailUnreadFiles(List<ControlFichero> listFicheros) throws ICEException;
 	
 	public void sendEmailFileResult(String nombreFichero) throws ICEException;
 }
