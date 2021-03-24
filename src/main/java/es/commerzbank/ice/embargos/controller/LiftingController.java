ab53da9481187e1ee92dd725c109a74af5fe2eda
alten_amassana <aniol.massana@alten.es>
2021-03-24 10:03:22 +0100

    Levantamientos: contabilizaciones automáticas /2

--- src/main/java/es/commerzbank/ice/embargos/controller/LiftingController.java
+++ src/main/java/es/commerzbank/ice/embargos/controller/LiftingController.java
@@ -261,7 +261,7 @@ public class LiftingController {
 
 			String userName = authentication.getName();
 		
-			accountingService.levantamientoContabilizarTx(codeFileControl, userName);
+			accountingService.levantamientoContabilizar(codeFileControl, userName);
 
 			//Se obtiene el fileControl que se va a retornar del Fichero Final:
 			resultFileControlDTO = fileControlService.getByCodeFileControl(codeFileControl);
