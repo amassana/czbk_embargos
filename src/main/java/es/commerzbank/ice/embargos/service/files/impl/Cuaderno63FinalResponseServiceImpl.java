4820c58bb13ef9a6886552e620250ad0df89c836
alten_amassana <aniol.massana@alten.es>
2021-04-12 18:44:22 +0200

    @Service annotation...

--- src/main/java/es/commerzbank/ice/embargos/service/files/impl/Cuaderno63FinalResponseServiceImpl.java
+++ src/main/java/es/commerzbank/ice/embargos/service/files/impl/Cuaderno63FinalResponseServiceImpl.java
@@ -23,10 +23,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.util.List;
 
+@Service
 public class Cuaderno63FinalResponseServiceImpl
     implements Cuaderno63FinalResponseService
 {
