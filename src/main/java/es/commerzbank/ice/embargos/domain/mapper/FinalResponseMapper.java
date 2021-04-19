6dbe4b99a1f2f171e8bc385225c20fbcf958ac53
alten_amassana <aniol.massana@alten.es>
2021-04-19 16:54:07 +0200

    F6 Mapper: holder

--- src/main/java/es/commerzbank/ice/embargos/domain/mapper/FinalResponseMapper.java
+++ src/main/java/es/commerzbank/ice/embargos/domain/mapper/FinalResponseMapper.java
@@ -14,10 +14,11 @@ public abstract class FinalResponseMapper {
 		@Mapping(source = "codResultadoEmbargo", target = "codeFinalResponse"),
 		@Mapping(source = "embargo.numeroEmbargo", target = "seizureNumber"),
 		@Mapping(source = "embargo.nif", target = "nif"),
+		@Mapping(source = "embargo.razonSocialInterna", target = "holder"),
 		@Mapping(source = "embargo.importe", target = "requestedSeizureAmount"),
 		@Mapping(source = "traba.importeTrabado", target = "seizedAmount"),
 		@Mapping(source = "totalLevantado", target = "liftedAmount"),
-		@Mapping(source = "totalNeto", target = "netAmount")
+		@Mapping(source = "totalNeto", target = "netAmount"),
 	})
 	public abstract FinalResponseDTO toFinalResponse(ResultadoEmbargo resultado);
 
